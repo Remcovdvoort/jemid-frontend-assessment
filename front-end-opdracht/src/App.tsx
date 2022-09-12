3f995374032942709bd081e67ce42f2488802d2d
Remcovdvoort <remcovandervoort00@gmail.com>
2022-09-12 15:14:47 +0200

    header + cards

--- front-end-opdracht/src/App.tsx
+++ front-end-opdracht/src/App.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import annie from "./img/Anniee.png";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState({});
+  const [plants, setPlants] = useState([]);
+  // const [error, setError] = useState({});
 
   interface Product {
     id: number;
@@ -14,62 +15,94 @@ function App() {
     diameter: number;
     standingPlace: string;
     wateringNeeds: string;
-    photoURL: string;
+    photoUrl: string;
   }
-  
-
 
   useEffect(() => {
-    
-    fetch('https://jemid-warmupapi.azurewebsites.net/api/products?pageSize=1&pageIndex=2')
-      .then(response => response.json())
-      .then(res => { 
-        console.log(res)
-        setProducts(res.products)
-      })
-      .catch(err => {
-        setError(err)
-      })
+    fetch(
+      "https://jemid-warmupapi.azurewebsites.net/api/products?pageSize=10&pageIndex=0"
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        setPlants(res.products);
+      });
+    // .catch((err) => {
+    //   setError(err);
+    // });
   }, []);
 
-
-
   return (
-      <>
-    <div className="App">
-      <ul className='Product'>
-        {
-          products.map((product:Product) =>
-          <li key={product.id}>
-            <h2>{product.name}</h2>
-            <h4>Beschrijving van product</h4>
-            <p>{product.description}</p>
-            <p>Prijs:{product.price}</p>
-            <p>{product.height}</p>
-            <p>{product.diameter}</p>
-            <p>{product.standingPlace}</p>
-            <p>{product.wateringNeeds}</p>
-            <img src={product.photoURL} alt="plant" />
-          </li>
-          )
-        }
-      </ul>
+    <>
+      <div className="App">
+        <header>
+          <h1>
+            <img className="Annie" src={annie} alt="" />
+          </h1>
+        </header>
 
-   
+        <main>
+
+          <div className="container">
+            <div className="sort">
+              <h2>Filteren op:</h2>
+              <select>
+                <option value="naam">Naam</option>
+                <option value="height">Hoogte</option>
+                <option value="diameter">Diameter</option>
+                <option value="standplaats">Standplaats</option>
+              </select>
+            </div>
+
+
+
+
+            <div className="product-grid">
+              {plants.map((plant: Product) => (
+                <div className="card stacked" key={plant.id}>
+                  <img className="card__img" src={plant.photoUrl} alt={plant.name} />
+                  <div className="card__content">
+                    <h2 className="card__title">{plant.name}</h2>
+                    <p className="card__price">€{plant.price}</p>
+                    <p>Hoogte: {plant.height}cm</p>
+                    <p>Diameter: {plant.diameter}cm</p>
+                  </div>
+                </div>
+              ))}
+
+
+
+
+            </div>
+          </div>
+        </main>
+
+        {/* <ul className="Planten_lijst">
+
+            {plants.map((product: Product) => (
+
+              <div className="Card" key={product.id}>
+
+                <img
+                  className="Photo"
+                  src={product.photoUrl}
+                  alt={product.name}
+                />
+
+                <div className="Text">
+                <h2>{product.name}</h2>
+                <p>Prijs: €{product.price}</p>
+                <p>Hoogte: {product.height}cm</p>
+                <p>Diameter: {product.diameter}cm</p>
+                </div>
+
+
+
+              </div>
+            ))}
+          </ul> */}
       </div>
-
-
-
-
-
-
-
-
-
-     
     </>
   );
-
 }
 
 export default App;
